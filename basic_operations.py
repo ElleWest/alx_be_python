403a1f7b342c65bef1ccef8d1c296434e423a708
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-09-14 18:06:03 +0200

    Fix: Use explicit arithmetic operations in print statements

--- basic_operations.py
+++ basic_operations.py
@@ -3,12 +3,7 @@
 number1 = 10
 number2 = 5
 
-# Perform arithmetic operations
-addition_result = number1 + number2
-subtraction_result = number1 - number2
-multiplication_result = number1 * number2
-
 # Print the results in the specified format
-print(f"Addition of {number1} and {number2} is {addition_result}")
-print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
-print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
