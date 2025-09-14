8e6683762ec2c7a033e7d120fd0fa955a147ef25
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-09-14 18:11:11 +0200

    Follow style guide: store operations in variables first, then print

--- basic_operations.py
+++ basic_operations.py
@@ -1,6 +1,12 @@
 number1 = 10
 number2 = 5
 
-print("Addition of", number1, "and", number2, "is", number1 + number2)
-print("Subtraction of", number1, "and", number2, "is", number1 - number2)
-print("Multiplication of", number1, "and", number2, "is", number1 * number2)
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
